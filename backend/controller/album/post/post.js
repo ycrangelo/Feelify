c55244a62e26cda23e326476a1786de4b3036b0a
ycrangelo <yocorangelo18@gmail.com>
2025-11-01 15:20:22 +0800

    added route   for  history  and  modify the   post route  of post

--- backend/controller/album/post/post.js
+++ backend/controller/album/post/post.js
@@ -1,24 +1,35 @@
 import prisma from "../../../prismaClient.js";
 
-export async function post(req,res){
-    try{
+export async function post(req, res) {
+  try {
+    const { dominantEmotion, picUrl, userId, createdBy, albumId, albumName } = req.body;
 
-        const {dominantEmotion,picUrl,userId,createdBy,albumId}  = req.body;
-        if(!dominantEmotion || !picUrl || !userId || !createdBy ||!albumId){
-            return res.status(401).json({ error: "Missing required field" });
-        }
-        const album = await prisma.album.create({
-            data: {
-                dominantEmotion:dominantEmotion,
-                picUrl:picUrl,
-                userId:userId,
-                createdBy:createdBy,
-                albumId:albumId
-            }
-        })
-        res.status(201).json({ data: album });
-    }catch(e){
-        console.error(e);
-        res.status(500).json({ error: "failed to post album" });
+    if (!dominantEmotion || !picUrl || !userId || !createdBy || !albumId || !albumName) {
+      return res.status(401).json({ error: "Missing required field" });
     }
+
+    const album = await prisma.album.create({
+      data: {
+        dominantEmotion,
+        albumName,
+        picUrl,
+        userId,
+        createdBy,
+        albumId,
+      },
+    });
+
+    const createHistory = await prisma.history.create({
+      data: {
+        userId,
+        createdBy,
+        verb: `created an album ${albumName}`,
+      },
+    });
+
+    res.status(201).json({ data: album });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Failed to post album" });
+  }
 }
