028b7aa2420b01fa067b77ab6c9660ef0cdf6561
ycrangelo <yocorangelo18@gmail.com>
2025-11-01 23:54:37 +0800

    delete ai related codes

--- frontend/tfSetup.ts
+++
@@ -1,8 +0,0 @@
-// tfSetup.ts
-import * as tf from '@tensorflow/tfjs';
-import '@tensorflow/tfjs-react-native';
-
-export const initTf = async () => {
-  await tf.ready(); // wait for TFJS runtime
-  console.log('✅ TensorFlow ready');
-};
